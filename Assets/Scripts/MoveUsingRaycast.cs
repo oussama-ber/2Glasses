0a1953d61d64d9c429682b17bdb287b4e4ab5353
Oussema <oussema.ber11@gmail.com>
2021-07-23 16:26:59 +0100

    Raycast movement v2

--- Assets/Scripts/MoveUsingRaycast.cs
+++ Assets/Scripts/MoveUsingRaycast.cs
@@ -5,27 +5,45 @@ using UnityEngine;
 
 public class MoveUsingRaycast : MonoBehaviour
 {
+MoveAlong moveAlongScript ; 
 
 [SerializeField] float range = 300f ;
 [SerializeField] float speed = 5f; 
-        RaycastHit hitForward;
-        RaycastHit hitBack;
-        RaycastHit hitRight;
-        RaycastHit hitLeft;
-          string name1 ; 
-        string name2 ; 
-        string name3 ; 
-        string name4 ; 
+[SerializeField] float SpeedAlong = 3.5f;
 
+Vector3 zeroVector = new Vector3 (0, 0, 0); 
+
+Vector3 playerPosition = new Vector3(0,0,0);
+public Collider collider; 
+public bool ShouldMove =  false;
+        bool notForward =false;
+        bool notBackk =false;
+        bool notRight =false;
+        bool notLeft =false;
+        bool boolForward =false;
+        bool boolBack =false;
+        bool boolRight =false;
+        bool boolLeft =false;
+
+    void Start() 
+    {
+        
+         moveAlongScript = GetComponent<MoveAlong>() ;
+        //  moveAlongScript.enabled = false ; 
+        // notBack =true;
+    }
      void FixedUpdate()
     {
        
             MoveToPlayer();
 
-    }
-    private void MoveToPlayer()
+            
+
+    }    private void MoveToPlayer()
     {
+
        TestRaycast();
+       
 
     }
   
@@ -33,7 +51,7 @@ public class MoveUsingRaycast : MonoBehaviour
     {
       
         Vector3 directionForward = transform.forward;
-        Vector3 directionBack = -transform.forward;
+        Vector3 directionBack = - transform.forward;
         Vector3 directionRight = transform.right;
         Vector3 directionLeft = -transform.right;
         Vector3 origin = transform.position; 
@@ -45,132 +63,158 @@ public class MoveUsingRaycast : MonoBehaviour
         Ray rayBack = new Ray(origin, directionBack);
         Ray rayRight = new Ray(origin, directionRight);
         Ray rayLeft = new Ray(origin, directionLeft);
-        Physics.Raycast(rayForward, out  hitForward, range);
-        Physics.Raycast(rayBack, out  hitBack, range);
-        Physics.Raycast(rayRight, out  hitRight, range);
-        Physics.Raycast(rayLeft, out  hitLeft, range);
-        if( Physics.Raycast(rayForward, out  hitForward, range))
+        RaycastHit hitForward;
+        RaycastHit hitBack;
+        RaycastHit hitRight;
+        RaycastHit hitLeft;
+
+        bool forwardBool =  Physics.Raycast(rayForward, out  hitForward, range);
+        bool backBool = Physics.Raycast(rayBack, out  hitBack, range);
+        bool rightBool = Physics.Raycast(rayRight, out  hitRight, range);
+        bool leftBool = Physics.Raycast(rayLeft, out  hitLeft, range);
+
+       
+        
+        // the last position of player
+        //Begin Player detection---------------------------------------------------------------------------------------------
+        //check if the ray detect something update the position of the player (target postion)
+        
+        if(forwardBool)
         {
+            boolForward = true;
             if(hitForward.transform.name == "BluePlayer")
             {
-                Debug.Log("forward");
-                transform.position += Time.deltaTime * speed * directionForward ; 
+                Debug.Log("moving forward");
+                playerPosition = hitForward.transform.position;
+                notForward =false; 
             }
-        } 
-        //  else 
-        // {
-        // Debug.Log("blablaablablalablbalbal");
-        // transform.position += Time.deltaTime * speed * directionForward ; 
-        // }
-        if(Physics.Raycast(rayBack, out  hitBack, range))
+            else {
+                notForward =true;
+                Debug.Log("notForward = " + notForward);
+            }
+        }
+        if(backBool)
         {
+            boolBack = true;
             if (hitBack.transform.name == "BluePlayer") 
             {
                 Debug.Log("back");
-                transform.position += Time.deltaTime * speed * directionBack ; 
+                // transform.position += Time.deltaTime * speed * directionBack ; 
+                 playerPosition = hitBack.transform.position; 
+                 notBackk = false;
+            }else
+            {
+                notBackk = true;
+                Debug.Log("notBack = " + notBackk);
             }
-        } 
-        //  else 
-        // {
-        // Debug.Log("blablaablablalablbalbal");
-        // transform.position += Time.deltaTime * speed * directionForward ; 
-        // }
-        if(Physics.Raycast(rayRight, out  hitRight, range))
+        }
+        if(rightBool)
         {
+            boolRight = true; 
             if (hitRight.transform.name == "BluePlayer") 
             {
                 Debug.Log("hitRight");
-                transform.position += Time.deltaTime * speed * directionRight ; 
+                // transform.position += Time.deltaTime * speed * directionRight ;
+                playerPosition = hitRight.transform.position; 
+                notRight= false; 
+            }else 
+            {
+                notRight= true;
+                Debug.Log("notRight = " + notRight);
             }
-        }  
-        //  else 
-        // {
-        // Debug.Log("blablaablablalablbalbal");
-        // transform.position += Time.deltaTime * speed * directionForward ; 
-        // }
-        if(Physics.Raycast(rayLeft, out  hitLeft, range))
+        }
+        if(leftBool)
         {
+            boolLeft = true;
             if (hitLeft.transform.name == "BluePlayer") 
             {
                 Debug.Log("hitLeft");
-                transform.position += Time.deltaTime * speed * directionLeft; 
-            }
+                // transform.position += Time.deltaTime * speed * directionLeft; 
+                 playerPosition = hitLeft.transform.position; 
+                    notLeft = false;
+            }else{
+                    notLeft = true;
+                    Debug.Log("notLeft = " + notLeft);
+                 }
         }
-        // else 
+        
+        // if (boolBack&&  boolForward&&  boolLeft&& boolRight)
         // {
-        // Debug.Log("blablaablablalablbalbal");
-        // transform.position += Time.deltaTime * speed * directionForward ; 
+        //     if(!(hitForward.transform.name == "BluePlayer") && !(hitBack.transform.name == "BluePlayer") && !(hitRight.transform.name == "BluePlayer") && !(hitLeft.transform.name == "BluePlayer"))
+        //         transform.position += Time.deltaTime * SpeedAlong * transform.forward;
         // }
-
-        // string name1 = hitForward.transform.tag; 
-        // string name2 = hitBack.transform.tag; 
-        // string name3 = hitRight.transform.tag; 
-        // string name4 = hitLeft.transform.tag; 
-         
-        // if (!( name1 == "BluePlayer") && !( name2 == "BluePlayer") && !( name3 == "BluePlayer")&& !( name4 == "BluePlayer"))
+        //End Player detection---------------------------------------------------------------------------------------------
+        if (transform.position == playerPosition)
+        {
+            playerPosition = zeroVector;
+        }
+        if ((playerPosition != zeroVector) )
+        {
+            moveAlongScript.enabled = false;  
+            transform.position = Vector3.MoveTowards(transform.position, playerPosition , Time.deltaTime * speed);
+        }else {
+            Debug.Log("make the moveAlongScript enable ");
+            moveAlongScript.enabled= true;
+        }
+        
+        // else
+        // if (((transform.position == playerPosition) || notForward && notBackk && notRight && notLeft))
         // {
-        //     // MoveAlong(hitForward, hitBack, hitRight, hitLeft);
-        //     transform.position += Time.deltaTime * speed * transform.right;
             
+        //     Debug.Log("...........................the cube must move forward");
+        //     transform.position += Time.deltaTime * SpeedAlong * transform.forward;
         // }
+        //check if the playpositon is not zero => go to target  
+        //Begin Check wall detectio*******************************************
+       
+        //End Check wall detection********************************************        
 
     }
-    private void MoveAlong( RaycastHit hitForward,  RaycastHit hitBack,  RaycastHit hitRight,  RaycastHit hitLeft)
-    {
-        // string name1 = hitForward.transform.tag; 
-        // string name2 = hitBack.transform.name; 
-        // string name3 = hitRight.transform.name; 
-        // string name4 = hitLeft.transform.name; 
+    // private void MoveAlong( RaycastHit hitForward,  RaycastHit hitBack,  RaycastHit hitRight,  RaycastHit hitLeft)
+    // {
+    //     // string name1 = hitForward.transform.tag; 
+    //     // string name2 = hitBack.transform.name; 
+    //     // string name3 = hitRight.transform.name; 
+    //     // string name4 = hitLeft.transform.name; 
 
-        RaycastHit _hitForward = hitForward;
-        RaycastHit _hitBack =hitBack ;
-        RaycastHit _hitRight = hitRight;
-        RaycastHit _hitLeft = hitLeft;
+    //     RaycastHit _hitForward = hitForward;
+    //     RaycastHit _hitBack =hitBack ;
+    //     RaycastHit _hitRight = hitRight;
+    //     RaycastHit _hitLeft = hitLeft;
 
-        float forwardDis = hitForward.distance ; 
-        float backDis = hitBack.distance;
-        float rightDis = hitRight.distance;
-        float leftDis = hitLeft.distance;
+    //     float forwardDis = hitForward.distance ; 
+    //     float backDis = hitBack.distance;
+    //     float rightDis = hitRight.distance;
+    //     float leftDis = hitLeft.distance;
 
-        float[] distances ={forwardDis, backDis , rightDis, leftDis};
+    //     float[] distances ={forwardDis, backDis , rightDis, leftDis};
 
-        RaycastHit[] raycastHits = {_hitForward, _hitBack, _hitRight, _hitLeft };
+    //     RaycastHit[] raycastHits = {_hitForward, _hitBack, _hitRight, _hitLeft };
 
 
         
-         RaycastHit directionToGo = _hitForward ; 
-          directionToGo.distance = 0f ; 
+    //      RaycastHit directionToGo = _hitForward ; 
+    //       directionToGo.distance = 0f ; 
           
 
-        for (int i = 0; i < raycastHits.Length; i++)
-        {
-            
-        if (directionToGo.distance < raycastHits[i].distance)
-            directionToGo = raycastHits[i];     
-        }
-        Debug.Log("check distance: the forwardDis" + _hitForward.distance + " backDis :"+ _hitBack.distance + " rightDis "+_hitRight.distance+ " leftDis "+_hitLeft.distance);
-        Debug.Log(" the longest path is : " + directionToGo.distance);
-
-    //    if (( name1 != "BluePlayer") && ( name2 != "BluePlayer") && ( name3 != "BluePlayer")&& ( name4 != "BluePlayer"))
+    //     for (int i = 0; i < raycastHits.Length; i++)
     //     {
-
-    //         transform.position += Time.deltaTime * speed * directionForward ; 
             
+    //     if (directionToGo.distance < raycastHits[i].distance)
+    //         directionToGo = raycastHits[i];     
     //     }
+    //     Debug.Log("check distance: the forwardDis" + _hitForward.distance + " backDis :"+ _hitBack.distance + " rightDis "+_hitRight.distance+ " leftDis "+_hitLeft.distance);
+    //     Debug.Log(" the longest path is : " + directionToGo.distance);
+
+    // //    if (( name1 != "BluePlayer") && ( name2 != "BluePlayer") && ( name3 != "BluePlayer")&& ( name4 != "BluePlayer"))
+    // //     {
+
+    // //         transform.position += Time.deltaTime * speed * directionForward ; 
+            
+    // //     }
         
-    }
+    // }
 
    
-     void OnCollisionEnter(Collision other) 
-     {
-        if (other.transform.tag == "RightWall")
-            transform.position += Time.deltaTime * speed * transform.forward;
-        if (other.transform.tag == "LeftWall")
-            transform.position += Time.deltaTime * speed * -transform.forward;
-        if (other.transform.tag == "TopWall")
-            transform.position += Time.deltaTime * -speed * transform.right;
-        if (other.transform.tag == "RightWall")
-            transform.position += Time.deltaTime * speed * transform.right;
-        
-    }
+     
 }
